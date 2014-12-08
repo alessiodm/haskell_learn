a093fc25a9a0e3f33dbbfcee109c31c7fa44af11
Alessio Della Motta <alessio@thousandeyes.com>
2014-12-07 23:16:39 -0800

    Some practice with high order functions

--- Types and Type Classes.md
+++ Types and Type Classes.md
@@ -7,6 +7,8 @@ You can examine the type of an expression in GHCi with `:t`.
 
 To call an infix function in a prefix form, we need to surround it with parenthesis.
 
+So a common pattern is to declare simple functions from the usual two parameters functions: (+3) is a function that takes a parameter and sums 3 to it (-> curried + function).
+
 Let's examine: `(==) :: (Eq a) => a -> a -> Bool`
 Everything before the => symbol is called a *class constraint*.
 We can read the previous type declaration like this: the equality function takes any two values that are of the same type and returns a Bool. The type of those two values must be a member of the Eq class (this was the class constraint).
--- functions5.hs
+++ functions5.hs
@@ -6,9 +6,9 @@ zipWith' _ [] _ = []
 zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 
-zipWith' (+) [4,2,5,6] [2,6,2,3]
-zipWith' max [6,3,2,1] [7,3,1,5]
-zipWith'' = zipWith' (*)
+--zipWith' (+) [4,2,5,6] [2,6,2,3]
+--zipWith' max [6,3,2,1] [7,3,1,5]
+--zipWith'' = zipWith' (*)
 
 --flip' :: (a -> b -> c) -> (b -> a -> c)
 --flip' f = g
@@ -16,3 +16,33 @@ zipWith'' = zipWith' (*)
 
 flip' :: (a -> b -> c) -> b -> a -> c  
 flip' f y x = f x y
+
+double a = a * 2
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (h:t) = f h : map' f t
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p l = [ x | x <- l, p x == True ]
+
+--q1 = head [ x | x <- [100000,99999..], mod x 3829 == 0]
+q1 = head (filter p [100000,99999..])
+     where p x = x `mod` 3829 == 0
+
+q2 = sum (takeWhile (< 10000) (filter odd (map (^2) [1..])))
+
+--cltzOp x = if x `mod` 2 == 0 then x `div` 2 else x * 3 + 1
+--cltz 1 = [1]
+--cltz n = n : cltz x
+--         where x = cltzOp n
+
+chain :: Integral a => a -> [a]
+chain 1 = [1]
+chain n
+    | even n = n:chain (n `div` 2)
+    | odd n  = n:chain (n * 3 + 1)
+
+numLongChains = length (filter isLong (map chain [1..100]))  
+    where isLong xs = length xs > 15 
