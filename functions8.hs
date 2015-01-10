59a619965d4d3e162c2ebb473dc6ff2489a3e13a
Alessio Della Motta <alessio.dellamotta@gmail.com>
2015-01-10 14:00:49 +0100

    Applicative Functors (second)

--- functions8.hs
+++ functions8.hs
@@ -2,6 +2,14 @@
 import Data.Char  
 import Data.List  
   
-main = do line <- fmap (intersperse '-' . reverse . map toUpper) getLine  
-          putStrLn line
+--main = do line <- fmap (intersperse '-' . reverse . map toUpper) getLine  
+--          putStrLn line
+
+class (Functor f) => Applicative' f where  
+    pure :: a -> f a  
+    (<*>) :: f (a -> b) -> f a -> f b
+
+instance Applicative' [] where
+    pure x = [x]
+    funcs <*> values = [ f v | f <- funcs, v <- values]
 
